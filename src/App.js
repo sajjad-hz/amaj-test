5bfe12f1c85f0ad071974933c9540985d033d1d3
sajjad-hz <s@jjad.dev>
2021-08-02 12:54:43 +0430

    menu, router, and structure changes
    menu and app routes.
    some structure changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.scss';
+import { Switch, Route } from "react-router-dom";
+import Home from './components/pages/Home/index'
+import MarektPlace from './components/pages/MarektPlace/index'
+import WalletBalance from './components/pages/WalletBalance/index'
+import Drops from './components/pages/Drops/index'
+import Accounts from './components/pages/Accounts/index'
+
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-<p>hiii</p>
+      <p>hiii</p>
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/market' component={MarektPlace} />
+        <Route path='/wallet' component={WalletBalance} />
+        <Route path='/drops' component={Drops} />
+        <Route path='/account' component={Accounts} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/generics/Header/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logoPic from '../../assets/icons/logo.png'
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <div className='logo-pic'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Logo
