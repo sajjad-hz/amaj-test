42bd9842daddc12c87799ef7d1617bb258ad1a11
sajjad-hz <s@jjad.dev>
2021-08-02 14:53:38 +0430

    home bottom without carousel

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,5 +1,6 @@
-import Button from '../../../utils/Button';
-import homePic from '../../assets/pictures/home-pic.png'
+import Button from "../../../utils/Button";
+import homePic from "../../assets/pictures/home-pic.png";
+import CalenderIcon from '../../assets/icons/Calendar.png'
 
 const Home = () => {
   return (
@@ -17,12 +18,26 @@ const Home = () => {
               history from discovering America through your birthday to creating
               Bitcoin.
             </p>
-      <Button className='btn-med' >GO TO MARKETPLACE</Button>
-
+            <Button className="btn-med">GO TO MARKETPLACE</Button>
           </div>
           <div className="home-top_right">
             <img src={homePic} alt="" />
           </div>
+        </div>
+
+        <div className="home-bottom">
+            <div className="home-cal">
+                <h4>The Premier Marketplace for Dates</h4>
+                <ul>
+                    <li className="home-cal__items">Sport</li>
+                    <li className="home-cal__items">Art</li>
+                    <li className="home-cal__items">History</li>
+                    <li className="home-cal__items">Personal</li>
+                </ul>
+                <div className="home-cal__icon">
+                    <img src={CalenderIcon} alt="cal icon" />
+                </div>
+            </div>
         </div>
       </div>
     </section>
